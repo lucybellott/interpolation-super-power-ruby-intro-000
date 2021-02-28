968f3144b145522a4ef4ee7fde208a4d49fd74b6
patient-controller-1876 <lucybellott@gmail.com>
2021-02-28 16:02:17 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,4 +3,4 @@ colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 def display_rainbow
   puts "R:[0], O:[1], Y:[2], G:[3], B:[4], I:[5], V:[6]"
 end
-display_rainbow
+display_rainbow(colors)
